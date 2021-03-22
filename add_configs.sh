1280c84de100dd49d008057dcdb9d92688bcf72a
Leigh MacDonald <leigh.macdonald@gmail.com>
2021-03-22 13:28:07 -0600

    Use hard links

--- add_configs.sh
+++ add_configs.sh
@@ -1,10 +1,10 @@
 echo "Adding configs from $1"
 for fn in $1/host_vars/*; do
-	ln -sf $fn host_vars
+	ln -f $fn host_vars
 	echo "Added hosts config: $fn"
 done
 
 for fn in $1/group_vars/*; do
-	ln -sf $fn group_vars
+	ln -f $fn group_vars
 	echo "Added group config: $fn"
 done
