e90395568f94f2f67b3f2eba3a3660d42adb17c0
Leigh MacDonald <leigh.macdonald@gmail.com>
2022-06-12 20:21:17 -0600

    Add custom sourcemod distro role to build.

--- roles/srcds/files/entry.sh
+++ roles/srcds/files/entry.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-bash ./srcds_run -debug -game "${STEAMAPP}" -console \
-                        -usercon \
-                        +fps_max "${SRCDS_FPSMAX}" \
-                        -tickrate "${SRCDS_TICKRATE}" \
-                        -port "${SRCDS_PORT}" \
-                        +clientport "${SRCDS_CLIENT_PORT}" \
-			+tv_port "${SRCDS_TV_PORT}" \
-                        +maxplayers "${SRCDS_MAXPLAYERS}" \
-                        +map "${SRCDS_STARTMAP}" \
-                        +sv_setsteamaccount "${SRCDS_TOKEN}" \
-                        +rcon_password "${SRCDS_RCONPW}" \
-                        +sv_password "${SRCDS_PW}" \
-                        +sv_region "${SRCDS_REGION}" \
-                        -ip "${SRCDS_IP}"
+bash ./srcds_run \
+  -game "${STEAMAPP}" \
+  -console \
+  -usercon \
+  +fps_max "${SRCDS_FPSMAX}" \
+  -tickrate "${SRCDS_TICKRATE}" \
+  -port "${SRCDS_PORT}" \
+  +clientport "${SRCDS_CLIENT_PORT}" \
+  +tv_port "${SRCDS_TV_PORT}" \
+  +maxplayers "${SRCDS_MAXPLAYERS}" \
+  +map "${SRCDS_STARTMAP}" \
+  +sv_setsteamaccount "${SRCDS_TOKEN}" \
+  +rcon_password "${SRCDS_RCONPW}" \
+  +sv_password "${SRCDS_PW}" \
+  +sv_region "${SRCDS_REGION}" \
+  -ip "${SRCDS_IP}"
