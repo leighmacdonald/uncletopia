a19ac2c2cee1e53160fb051e538c3409cb32e657
Leigh MacDonald <leigh.macdonald@gmail.com>
2026-07-26 18:35:25 -0600

    Add mojave/premuda map configs

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.11";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-26.05";
 
   pkgs = import nixpkgs {
     config = {};
