e8b4db2e16089e746147d4ac125fcf972cfde158
Leigh MacDonald <leigh.macdonald@gmail.com>
2026-09-06 02:22:35 -0600

    Use nix-sourcemod

--- flake.nix
+++ flake.nix
@@ -4,55 +4,21 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     flake-utils.url = "github:numtide/flake-utils";
+    nix-sourcemod.url = "github:leighmacdonald/nix-sourcemod";
   };
 
   outputs =
     {
       nixpkgs,
       flake-utils,
+      nix-sourcemod,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-
-        sourcemod = pkgs.stdenv.mkDerivation {
-          pname = "sourcemod";
-          version = "1.12.0-git7253";
-
-          src = pkgs.fetchurl {
-            # master
-            # url = "https://github.com/alliedmodders/sourcemod/releases/download/1.13.0.7456/sourcemod-1.13.0-git7456-linux.tar.gz";
-            # sha256 = "sha256-9rDtxNb4At/cSSjMsP2f1Xpc01vzGquAjW7RMr+FtQM=";
-            # url = "https://github.com/alliedmodders/sourcemod/releases/download/1.13.0.7453/sourcemod-1.13.0-git7453-linux.tar.gz";
-            # sha256 = "sha256-m86ukkuWP9nBvEPprltzgLFyAxCg11++2zLyUmG4aE4=";
-            # v9
-            # url = "https://github.com/alliedmodders/sourcemod/releases/download/1.13.0.7441/sourcemod-1.13.0-git7441-linux.tar.gz";
-            # sha256 = "sha256-dEvfmAnluzLPC+xmMv6AXUspLoU24hbj/fC1kZVPzCc=";
-            # stable-1.12
-            url = "https://github.com/alliedmodders/sourcemod/releases/download/1.12.0.7253/sourcemod-1.12.0-git7253-linux.tar.gz";
-            sha256 = "sha256-a7yrmJzaCtqDYA0Nyw9Gr/0WUpsrAu0tuhtbrq8C/bQ=";
-          };
-
-          dontConfigure = true;
-          dontBuild = true;
-          dontUnpack = true;
-
-          installPhase = ''
-            mkdir -p $out
-            tar xzf $src
-            mv ./* $out/
-          '';
-
-          meta = {
-            description = "SourceMod - Source engine scripting and administration";
-            homepage = "https://www.sourcemod.net/";
-            license = pkgs.lib.licenses.gpl3Plus;
-            platforms = pkgs.lib.platforms.linux;
-            mainProgram = "spcomp64";
-          };
-        };
+        sourcemod = nix-sourcemod.packages.${system}.sourcemod_stable;
       in
       {
         devShells.default = pkgs.mkShell {
